4d76db8a30a96abe4bb1578c3c51c9d4105392b3
bigwhale1948@gmail.com <bigwhale1948@gmail.com>
2022-11-14 22:30:54 +0900

    [221114] 로그인 세션 찾기

--- src/main/java/com/ward/photogram/web/ImageController.java
+++ src/main/java/com/ward/photogram/web/ImageController.java
@@ -1,18 +1,22 @@
 package com.ward.photogram.web;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+@RequiredArgsConstructor
 @Controller
 public class ImageController {
 
-    @GetMapping({"/","/image/story"})
-    public String story(){
+
+    @GetMapping({"/", "/image/story"})
+    public String story() {
+
         return "image/story";
     }
 
-    @GetMapping({"/","/image/popular"})
-    public String popular(){
+    @GetMapping("/image/popular")
+    public String popular() {
+
         return "image/popular";
     }
 }
