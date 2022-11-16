f963f34a4c1a8672ac30f752a61b497763288eb0
bigwhale1948@gmail.com <bigwhale1948@gmail.com>
2022-11-16 21:46:48 +0900

    [221116] 구독하기 연관관계 설정

---
+++ src/main/java/com/ward/photogram/domain/subscribe/SubscribeRepository.java
@@ -0,0 +1,10 @@
+package com.ward.photogram.domain.subscribe;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+public interface SubscribeRepository extends JpaRepository<Subscribe, Long> {
+
+
+}
--- src/main/java/com/ward/photogram/domain/user/User.java
+++ src/main/java/com/ward/photogram/domain/user/User.java
@@ -17,22 +17,30 @@ import java.time.LocalDateTime;
 @Data
 @Entity // 디비에 테이블을 생성
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)//번호 증가 전략이데이터베이스를 따라간다
+    @Id // primary Key 설정
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 번호 증략 전략을 데이터베이스를 따라간다.
     private int id;
-    @Column(length =20,unique = true)
+
+
+    @Column(length = 15, unique = true) //제약조건 DB에 들어갈 때, 길이 제한 및 Unique
     private String username;
+
+    @Column(nullable = false) //빈 데이터 못들어오게 막음
     private String password;
 
-    private String name;
-    private String website;
-    private String bio;
+    @Column(nullable = false) //빈 데이터 못들어오게 막음
     private String email;
+
+    @Column(nullable = false) //빈 데이터 못들어오게 막음
+    private String name;
+
+    private String website; //웹사이트
+    private String bio; //자기소개
     private String phone;
     private String gender;
 
-    private String profileImageUrl;
-    private String role;
+    private String profileImageUrl; //유저 사진
+    private String role; //권한
 
 
     private LocalDateTime createDate;
